90089e6dd8ee1a99a6fcee08fca15e7a1473542d
Goosewiz <IGusevDanila@gmail.com>
2023-11-09 12:43:10 +0400

    Task 4 commit

--- Task4/src/Task4/Main.java
+++ Task4/src/Task4/Main.java
@@ -9,33 +9,32 @@ public class Main {
 		if (sc.hasNextInt()) {
 			n = sc.nextInt();
 		}
-		int[] array = new int[n];
-		for (int i = 0; i < n; i++) {
-			array[i] = -1000 + (int)(Math.random()*2000);
-			System.out.print(array[i] + " ");
-		}
-		System.out.println(" ");
-		int count = 0;
+		int condition;
+		int count = 1;
 		int answer = 0;
-		int condition = 0;
-		if (array[0]>0)
+		int k = 1;
+		int number = -1000 + (int) (Math.random() * 2000);
+		System.out.print(number + " ");
+		if (number > 0)
 			condition = 1;
-		else if (array[0]<0)
+		else if (number < 0)
 			condition = -1;
 		else
 			condition = 0;
-		for (int i = 0; i < n; i++) {
-			if (array[i]>0 && condition>0) {
+		do {
+			number = -1000 + (int) (Math.random() * 2000);
+			System.out.print(number + " ");
+			if (number > 0 && condition > 0) {
 				count = count + 1;
-			} else if (array[i]>0 && condition<0){
+			} else if (number > 0 && condition < 0) {
 				if (count > answer) {
 					answer = count;
 				}
 				count = 1;
 				condition = 1;
-			} else if (array[i]<0 && condition<0){
+			} else if (number < 0 && condition < 0) {
 				count = count + 1;
-			} else if (array[i]<0 && condition>0){
+			} else if (number < 0 && condition > 0) {
 				if (count > answer) {
 					answer = count;
 				}
@@ -46,12 +45,18 @@ public class Main {
 					answer = count;
 				}
 				count = 0;
-				condition = 0;
+				if (number > 0)
+					condition = 1;
+				else if (number < 0)
+					condition = -1;
+				else
+					condition = 0;
 			}
-		}
+			k++;
+		} while (k < n);
 		if (count > answer)
 			answer = count;
-		System.out.println(answer);
+		System.out.println();
 		System.out.println(answer);
 	}
 }
