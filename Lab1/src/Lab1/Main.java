62675dc980c4b28ecb798cfaf7271edfe517303a
Goosewiz <IGusevDanila@gmail.com>
2023-11-10 21:40:04 +0400

    Task1 update

--- Lab1/src/Lab1/Main.java
+++ Lab1/src/Lab1/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		int[][] table = new int[3][8];
 		double[] arr = { 30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675 };
+		String[] array = { "<- Стандартное округление int", "<- Округление вниз", "<- Округление вверх" };
 		for (int i = 0; i < table[0].length; i++) {
 			table[0][i] = (int) Math.round(arr[i]);
 			table[1][i] = (int) Math.floor(arr[i]);
@@ -13,11 +14,13 @@ public class Main {
 			for (int j = 0; j < table[0].length; j++) {
 				System.out.print(table[i][j] + "\t");
 			}
+			System.out.print(array[i] + '\t');
 			System.out.println();
 		}
 		for (int i = 0; i < table[0].length; i++) {
 			long answer = Math.round(arr[i]);
 			System.out.print(answer + "\t");
 		}
+		System.out.print("<- Стандартное округление long" + '\t');
 	}
 }
