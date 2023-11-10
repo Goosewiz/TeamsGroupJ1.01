1536eb1eb7bfed83a32cd38070c1d89feb21be7a
Goosewiz <IGusevDanila@gmail.com>
2023-11-10 20:09:19 +0400

    Task1 update

--- Lab1/src/Lab1/Main.java
+++ Lab1/src/Lab1/Main.java
@@ -4,17 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		int[][] table = new int[3][8];
 		double[] arr = { 30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675 };
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < table[0].length; i++) {
 			table[0][i] = (int) Math.round(arr[i]);
 			table[1][i] = (int) Math.floor(arr[i]);
 			table[2][i] = (int) Math.ceil(arr[i]);
 		}
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 8; j++) {
-				System.out.print(table[i][j]);
-				System.out.print("   ");
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[0].length; j++) {
+				System.out.print(table[i][j] + "\t");
 			}
 			System.out.println();
+		}
+		for (int i = 0; i < table[0].length; i++) {
+			long answer = Math.round(arr[i]);
+			System.out.print(answer + "\t");
 		}
 	}
 }
